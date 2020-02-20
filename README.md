8487ca6316652f8ef99a1cf1784101a4581b2594
Csaba Koncz <ckoncz@cloudera.com>
2020-02-20 16:28:52 +0100

    enable classpath sorting

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
-Run embedded Jetty:
+### Run Jetty embedded via Java code:
 
 ```
 mvn compile exec:java -Dexec.mainClass=com.ck.webapp1.Main -Dexec.classpathScope=compile
 ```
 
-Run with Jetty plugin:
+### Launch with Jetty plugin:
 ```
 mvn package jetty:deploy-war
 ```
- 
+
+### Run without packaging all the time
+`webapp1-runner` does not have the webapp as dependency, so packaging it will not package webapp1 (saves time, and allows you to change thw war before launching Jetty.
+```
+mvn package
+mvn -am -pl webapp1-runner package jetty:deploy-war
+```
+
+### Classpath sorting
+System property `myclassloader.sort` with values `asc` or `desc` triggers classpath sorting.
+
+Sort the jars in ascending alphabetic order:
+```
+mvn -Dmyclassloader.sort=asc -am -pl webapp1-runner package jetty:deploy-war
+```
--- myclassloader1/src/main/java/com/ck/myclassloader1/MyClassloader.java
+++ myclassloader1/src/main/java/com/ck/myclassloader1/MyClassloader.java
@@ -1,7 +1,12 @@
 package com.ck.myclassloader1;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Locale;
 
+import org.eclipse.jetty.util.StringUtil;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.resource.Resource;
@@ -9,9 +14,11 @@ import org.eclipse.jetty.webapp.WebAppClassLoader;
 
 public class MyClassloader extends WebAppClassLoader {
 	private static final Logger LOG = Log.getLogger(MyClassloader.class);
+	private String sortMode;
 
 	public MyClassloader(ClassLoader parent, Context context) throws IOException {
 		super(parent, context);
+		this.sortMode = System.getProperty("myclassloader.sort","off");
 	}
 
 	public MyClassloader(Context context) throws IOException {
@@ -27,6 +34,67 @@ public class MyClassloader extends WebAppClassLoader {
 	@Override
 	public void addJars(Resource lib) {
 		LOG.info("addJars {}", lib);
-		super.addJars(lib);
+		addJars0(lib);
+	}
+	
+    private boolean isFileSupported0(String file)
+    {
+    	Method method;
+		try {
+			method = WebAppClassLoader.class.getDeclaredMethod("isFileSupported", String.class);
+			method.setAccessible(true);
+			return (Boolean) method.invoke(this, file);
+		} catch (Exception e) {
+			throw new RuntimeException("isFileSupported", e);
+		}
+    }
+    
+	
+    public void addJars0(Resource lib)
+    {
+        if (lib.exists() && lib.isDirectory())
+        {
+            String[] files=lib.list();
+            sort(files);
+            for (int f=0;files!=null && f<files.length;f++)
+            {
+                try 
+                {
+                    Resource fn=lib.addPath(files[f]);
+                    if(LOG.isDebugEnabled())
+                        LOG.debug("addJar - {}", fn);
+                    String fnlc=fn.getName().toLowerCase(Locale.ENGLISH);
+                    // don't check if this is a directory, see Bug 353165
+                    if (isFileSupported0(fnlc))
+                    {
+                        String jar=fn.toString();
+                        jar=StringUtil.replace(jar, ",", "%2C");
+                        jar=StringUtil.replace(jar, ";", "%3B");
+                        addClassPath(jar);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LOG.warn(Log.EXCEPTION,ex);
+                }
+            }
+        }
+    }
+
+	private void sort(String[] files) {
+		
+		LOG.info("MyClassloader sort is {}", this.sortMode);
+
+		switch (this.sortMode) {
+		case "asc":
+			Arrays.sort(files);
+			break;
+		case "desc":
+			Arrays.sort(files, Collections.reverseOrder());
+			break;
+		default:
+			LOG.info("MyClassloader does not change classpath order");
+			break;
+		}
 	}
 }
